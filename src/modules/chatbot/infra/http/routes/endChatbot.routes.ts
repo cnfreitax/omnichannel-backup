48576ad2895a562ee5fc68d4f43aeb0f11c83190
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-08-26 15:07:34 -0300

    Cricao de rotas para o servico de end of chatbot message

---
+++ src/modules/chatbot/infra/http/routes/endChatbot.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { Segments, Joi, celebrate } from 'celebrate';
+
+import EndOfChatbotController from '@modules/chatbot/infra/http/controllers/EndOfChatbotController';
+
+const endOfChatbotController = new EndOfChatbotController();
+
+const endChatbotRouter = Router();
+
+endChatbotRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      text: Joi.string().required(),
+      type: Joi.string().required(),
+      parent_id: Joi.string().optional(),
+    },
+  }),
+  endOfChatbotController.create,
+);
+
+export default endChatbotRouter;
--- src/modules/chatbot/infra/http/routes/index.ts
+++ src/modules/chatbot/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import greetingRouter from '@modules/chatbot/infra/http/routes/greeting.routes';
 import surveyRouter from '@modules/chatbot/infra/http/routes/survey.routes';
+import endChabotRouter from '@modules/chatbot/infra/http/routes/endChatbot.routes';
 
 const routes = Router();
 
 routes.use('/greeting', greetingRouter);
 routes.use('/survey', surveyRouter);
+routes.use('/end-chatbot', endChabotRouter);
 
 export default routes;
