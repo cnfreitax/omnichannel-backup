d0b4051e1011579f1cc7ffa71e0bdefc1ec22d0f
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-08-27 16:59:37 -0300

    feat: create sector entity

---
+++ src/modules/company/repositories/ISectorRepository.ts
@@ -0,0 +1,9 @@
+import Sector from '@modules/company/infra/typeorm/entities/Sector';
+import ICreateSectorDTO from '@modules/company/dtos/ICreateSectorDTO';
+
+export default interface ISectorRepository {
+  findById(id: string): Promise<Sector | undefined>;
+  findAllCompanySectors(company_id: string): Promise<Sector[] | undefined>;
+  create(data: ICreateSectorDTO): Promise<Sector>;
+  save(sector: Sector): Promise<Sector>;
+}
---
+++ src/shared/infra/typeorm/migrations/1598540355913-CreateSectors.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateSectors1598540355913 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'sectors',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true,
+          },
+          { name: 'label', type: 'varchar' },
+          { name: 'phone', type: 'varchar' },
+          { name: 'company_id', type: 'varchar' },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+          { name: 'updated_at', type: 'timestamp', default: 'now()' },
+        ],
+        foreignKeys: [
+          {
+            name: 'SectorCompany',
+            referencedTableName: 'companies',
+            referencedColumnNames: ['id'],
+            columnNames: ['company_id'],
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('sectors');
+  }
+}
