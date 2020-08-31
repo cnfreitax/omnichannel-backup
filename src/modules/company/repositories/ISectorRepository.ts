484511362c7ff610d3d260264235639cb49643dc
Victor Freitas <victornobrega1980@gmail.com>
2020-08-31 13:47:32 -0300

    feat: ensure CreateSectorService don't create sector if secotr alredy exists

--- src/modules/company/repositories/ISectorRepository.ts
+++ src/modules/company/repositories/ISectorRepository.ts
@@ -3,7 +3,7 @@ import ICreateSectorDTO from '@modules/company/dtos/ICreateSectorDTO';
 
 export default interface ISectorRepository {
   findById(id: number): Promise<Sector | undefined>;
-  findByName(name: string): Promise<Sector | undefined>;
+  findSectorCompany(label: string, company_id: number): Promise<Sector | undefined>;
   findAllCompanySectors(company_id: number): Promise<Sector[] | undefined>;
   create(data: ICreateSectorDTO): Promise<Sector>;
   save(sector: Sector): Promise<Sector>;
