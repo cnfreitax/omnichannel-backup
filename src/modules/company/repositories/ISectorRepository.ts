cc6f06459700686420cc11f4b0e5a2ed7a549cd2
Victor Freitas <victornobrega1980@gmail.com>
2020-08-31 13:31:30 -0300

    feat: ensure CreateSectorService not registered sector duplicated

--- src/modules/company/repositories/ISectorRepository.ts
+++ src/modules/company/repositories/ISectorRepository.ts
@@ -3,6 +3,7 @@ import ICreateSectorDTO from '@modules/company/dtos/ICreateSectorDTO';
 
 export default interface ISectorRepository {
   findById(id: number): Promise<Sector | undefined>;
+  findByName(name: string): Promise<Sector | undefined>;
   findAllCompanySectors(company_id: number): Promise<Sector[] | undefined>;
   create(data: ICreateSectorDTO): Promise<Sector>;
   save(sector: Sector): Promise<Sector>;
