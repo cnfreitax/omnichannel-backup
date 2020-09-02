15edd07ef7e86f0e6ae04c46faa6b7a31c96d6eb
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-09-02 16:43:32 -0300

    refactor: fix imports

---
+++ src/modules/chatbot/services/ListAllCompanyContainersService.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'tsyringe';
+
+import ICompanyRepository from '@modules/company/repositories/ICompanyRepository';
+import AppError from '@shared/errors/AppError';
+import IOptionRepository from '../repositories/IOptionRepository';
+import Container from '../infra/typeorm/entities/Container';
+import IContainerRepository from '../repositories/IContainerRepository';
+
+@injectable()
+export default class ListAllCompanyContainersService {
+  constructor(
+    @inject('OptionRepository')
+    private optionRepository: IOptionRepository,
+
+    @inject('ContainerRepository')
+    private containerRepository: IContainerRepository,
+
+    @inject('CompaniesRepository')
+    private companyRepository: ICompanyRepository,
+  ) {}
+
+  public async execute(company_id: number): Promise<Container[]> {
+    const company = await this.companyRepository.findById(company_id);
+
+    if (!company) {
+      throw new AppError('Company not found');
+    }
+
+    const containers = await this.containerRepository.listAllCompanyContainers(company_id);
+
+    return containers;
+  }
+}
