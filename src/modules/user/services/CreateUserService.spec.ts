c10f1e3c2417176f2ba9bb264ae8aa385878fe06
Victor Freitas <victornobrega1980@gmail.com>
2020-09-03 16:50:56 -0300

    refactor: remove duplicated code

--- src/modules/user/services/CreateUserService.spec.ts
+++ src/modules/user/services/CreateUserService.spec.ts
@@ -1,41 +1,46 @@
 import AppError from '@shared/errors/AppError';
 import FakeHashProvider from '@modules/user/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUserRepository from '@modules/user/repositories/fakes/FakeUserRepository';
-import CreateUser from './CreateUserService';
+import CreateUserService from './CreateUserService';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+  access_level: string;
+}
+
+const makeFakeUser = (): IRequest => ({
+  name: 'any_name',
+  email: 'any@mail.com',
+  password: 'any_password',
+  access_level: 'adm',
+});
+
+let fakeUserRepository: FakeUserRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
 
 describe('Create User Service', () => {
+  beforeEach(() => {
+    fakeUserRepository = new FakeUserRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUserRepository, fakeHashProvider);
+  });
   it('Should be able to create a new Company', async () => {
-    const fakeUserRepository = new FakeUserRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUser(fakeUserRepository, fakeHashProvider);
-
-    const user = await createUser.execute({
-      email: 'jj@email.com',
-      name: 'Eduardo',
-      password: '123456',
-      access_level: 'adm',
-    });
+    const user = await createUser.execute(makeFakeUser());
 
     expect(user).toHaveProperty('id');
   });
 
-  it('Should not be able create two Company with the same email', async () => {
-    const fakeUserRepository = new FakeUserRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUser(fakeUserRepository, fakeHashProvider);
-
-    await createUser.execute({
-      email: 'jj@email.com',
-      name: 'Eduardo',
-      password: '123456',
-      access_level: 'adm',
-    });
+  it('Should not be able create two Users with the same email', async () => {
+    await createUser.execute(makeFakeUser());
 
     expect(
       createUser.execute({
-        email: 'jj@email.com',
-        name: 'Eduardo',
-        password: '123456',
+        email: 'any@mail.com',
+        name: 'any_name',
+        password: 'any_password',
         access_level: 'adm',
       }),
     ).rejects.toBeInstanceOf(AppError);
