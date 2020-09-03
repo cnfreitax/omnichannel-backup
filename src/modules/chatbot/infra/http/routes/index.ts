5bf152179055f10b433a872b371248176d734535
Victor Freitas <victornobrega1980@gmail.com>
2020-09-03 11:26:10 -0300

    refactor: route to option

--- src/modules/chatbot/infra/http/routes/index.ts
+++ src/modules/chatbot/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import containerRouter from '@modules/chatbot/infra/http/routes/container.routes
 import greetingRouter from '@modules/chatbot/infra/http/routes/greeting.routes';
 import surveyRouter from '@modules/chatbot/infra/http/routes/survey.routes';
 import endChabotRouter from '@modules/chatbot/infra/http/routes/endChatbot.routes';
+import optionRouter from './option.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/container', containerRouter);
 routes.use('/greeting', greetingRouter);
 routes.use('/survey', surveyRouter);
 routes.use('/end-chatbot', endChabotRouter);
+routes.use('/option', optionRouter);
 
 export default routes;
