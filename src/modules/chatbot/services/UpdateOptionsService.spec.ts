e3944d852be5af175dec7ca3d453918a2ada58e7
Victor Freitas <victornobrega1980@gmail.com>
2020-09-10 09:21:19 -0300

    test: ensure UpdateOptionService not uptation destiny conection if invalid value is provided

--- src/modules/chatbot/services/UpdateOptionsService.spec.ts
+++ src/modules/chatbot/services/UpdateOptionsService.spec.ts
@@ -37,7 +37,7 @@ describe('UpdateOption Service', () => {
     expect(option.description).toBe('new_description');
   });
 
-  test('Should returns trows if Option not found', async () => {
+  test('Should returns throws if Option not found', async () => {
     await expect(updateOption.execute({ optionId: 0, description: 'new_description' })).rejects.toBeInstanceOf(AppError);
   });
 
@@ -51,5 +51,11 @@ describe('UpdateOption Service', () => {
     const option = await createOption.execute(makeOptionRequest(container.id));
     await updateOption.execute({ optionId: option.id, to: containerDestiny.id, description: 'any_description' });
     expect(option.to).toEqual(containerDestiny.id);
+  });
+
+  test('Should returns throws if invalid destiny connectiom is provided', async () => {
+    const container = await fakeContainerRepository.create(makeFakeRequestContainer());
+    const option = await createOption.execute(makeOptionRequest(container.id));
+    await expect(updateOption.execute({ optionId: option.id, to: 100, description: 'any_description' })).rejects.toBeInstanceOf(AppError);
   });
 });
