db051180c9bc9a2c1789389d800f45f5fc78ea35
Victor Freitas <victornobrega1980@gmail.com>
2020-09-10 15:16:47 -0300

    refactor: remove app responsibilities from server

--- src/shared/infra/http/config/app.ts
+++ src/shared/infra/http/config/app.ts
@@ -1,7 +1,28 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import uploadConfig from '@config/upload';
+import { errors } from 'celebrate';
+import AppError from '@shared/errors/AppError';
 import setupMiddlewares from './middlewares';
+import router from '../../routes';
 
 const app = express();
 setupMiddlewares(app);
+app.use(errors());
+app.use(router);
 
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Server Error',
+  });
+});
+
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 export default app;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,11 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import { errors } from 'celebrate';
-import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
-import routes from './routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
 import app from './config/app';
 
 const port = process.env.PORT || 3333;
 
-app.use(routes);
-app.use(errors());
-
-app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: 'Server Error',
-  });
-});
-
-app.use('/files', express.static(uploadConfig.uploadsFolder));
-
-app.listen(port);
+// eslint-disable-next-line
+app.listen(port, () => console.log('Server started'));
