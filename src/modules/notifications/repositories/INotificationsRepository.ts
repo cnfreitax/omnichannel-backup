10cd4aa295a289cd573903d68141f029f968ab30
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-09-16 15:26:50 -0300

    feat: remove unused module notifications

--- src/modules/notifications/repositories/INotificationsRepository.ts
+++
@@ -1,6 +0,0 @@
-import ICreateNotificationDTO from '@modules/notifications/dtos/INotificationsRepositoryDTO';
-import Notification from '@modules/notifications/infra/typeorm/schemas/Notifcation';
-
-export default interface INotificationRepository {
-  create(data: ICreateNotificationDTO): Promise<Notification>;
-}
