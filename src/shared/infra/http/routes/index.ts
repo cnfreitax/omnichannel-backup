3d2aef92687542fb4950ef2fcb3f6d8c654e471a
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-09-17 09:00:36 -0300

    refactor: routing tests

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,9 +5,10 @@ import companyRouter from '@modules/company/infra/http/routes';
 import messageRouter from '@modules/messageHandler/infra/http/routes/message.routes';
 
 const router = Router();
-const routesList = [chatbotRoutes, userRoutes, companyRouter];
+const routesList = [chatbotRoutes, companyRouter];
 
 router.use('/', messageRouter);
+router.use('/', userRoutes);
 for (const route of routesList) {
   router.use('/api', route);
 }
