ecf54ac449c41d3ca6409fd711cc665742878446
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-09-18 16:02:43 -0300

    refactor: add fake first message to user

--- src/shared/container/providers/MessageProvider/implementations/SendMessageProvider.ts
+++ src/shared/container/providers/MessageProvider/implementations/SendMessageProvider.ts
@@ -4,6 +4,15 @@ import IMessageProvider from '../models/IMessageProvider';
 
 export default class SendMessageProvider implements IMessageProvider {
   public async send(data: Array<ISendMessageDTO>): Promise<void> {
+    let messagesToSend = data;
+
+    messagesToSend.unshift({
+      token: data[0].token,
+      Telephone: data[0].Telephone,
+      codCampaign: data[0].codCampaign,
+      Message: '',
+    });
+
     await Axios.post(process.env.WEB_HOOK_RESPONSE || 'default', data);
   }
 }
