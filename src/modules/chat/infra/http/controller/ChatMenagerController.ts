cffa83c2a221039050c8986f1f880ed2e82cb4f8
Joao Rodrigues PC-Senai <victor.rodrigues@sp.senai.br>
2020-09-30 16:36:36 -0300

    refactor: format chatId to Number

--- src/modules/chat/infra/http/controller/ChatMenagerController.ts
+++ src/modules/chat/infra/http/controller/ChatMenagerController.ts
@@ -8,8 +8,9 @@ export default class ChatMenagerController {
     const attendantId = req.user.id;
     const { chatId } = req.body;
     const chatIdFormat = Number(chatId);
+    const attendantIdFormat = Number(attendantId);
     const selectChat = container.resolve(SelectChatService);
-    await selectChat.execute({ attendantId, chatId: chatIdFormat });
+    await selectChat.execute({ attendantId: attendantIdFormat, chatId: chatIdFormat });
     return res.send(200).json(selectChat);
   }
 
