c3d968d6d3a604b046fbdcebebd202f9c3969674
Victor Freitas <victor.nobrega@sp.senai.br>
2020-09-30 13:38:10 -0300

    feat: ensure ExitChatService delete sutomer stage

--- src/modules/chat/service/ExitChatService.ts
+++ src/modules/chat/service/ExitChatService.ts
@@ -1,3 +1,4 @@
+import ICustomerStageRepository from '@modules/messageHandler/repository/ICustomerStage';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import IChatlineRepository from '../repository/IChatlineRepository';
@@ -7,6 +8,9 @@ export default class ExitChatService {
   constructor(
     @inject('ChatlineRepository')
     private chatlineRepository: IChatlineRepository,
+
+    @inject('CustomerStageRepository')
+    private customerStageRepository: ICustomerStageRepository,
   ) {}
 
   public async execute(chatId: number): Promise<void> {
@@ -15,5 +19,14 @@ export default class ExitChatService {
       throw new AppError('Error, try again');
     }
     await this.chatlineRepository.deleteChatline(chatId);
+
+    const costumer = chatSelected.customer_id;
+    const company = chatSelected.company_id;
+
+    const customerStage = await this.customerStageRepository.findStage(company, costumer);
+    if (!customerStage) {
+      throw new AppError('Stage not found');
+    }
+    await this.customerStageRepository.deleteStage(customerStage.id);
   }
 }
