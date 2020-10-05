54e721085ceb54aa1c9fd8bef5819dfd7862437a
Victor Freitas <victor.nobrega@sp.senai.br>
2020-10-05 09:43:58 -0300

    feat: ensure only adm user can create an new user

--- src/modules/user/infra/http/routes/user.routes.ts
+++ src/modules/user/infra/http/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { Segments, Joi, celebrate } from 'celebrate';
 import UserController from '@modules/user/infra/http/controllers/UserController';
+import ensureAuthenticatedAdmUser from '../middlewares/ensureAuthencticatedAdmUser';
 
 const userController = new UserController();
 const userRouter = Router();
+userRouter.use(ensureAuthenticatedAdmUser);
 
 userRouter.post(
   '/:access_level',
