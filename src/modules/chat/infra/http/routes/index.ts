32d54cd669eccb710d878f0c847bec57a900fb8b
Victor Freitas <victor.nobrega@sp.senai.br>
2020-10-05 09:15:50 -0300

    fix: route get chat

--- src/modules/chat/infra/http/routes/index.ts
+++ src/modules/chat/infra/http/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import handleChat from './handleChat';
 import listUserAvailableRouter from './listAvailable';
-import listlistChatRouter from './listChatsFromCompany';
+import listChatRouter from './listChatsFromCompany';
 import selectChatRouter from './selectChat.routes';
 
 const routes = Router();
 routes.use('/available', listUserAvailableRouter);
-routes.use('/chat', listlistChatRouter);
+routes.use('/chat', listChatRouter);
 routes.use('/union/response', handleChat);
 routes.use('/select', selectChatRouter);
 
